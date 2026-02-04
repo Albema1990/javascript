7cf4ea3ff0601a48fef020e0e5cffa5b84320c7c
Álvaro Benitez Mayorga <alvarobenitezmayorga@gmail.com>
2026-02-04 21:52:27 +0100

    variables y tipos de datos

--- clase-01/js/scripts.js
+++ clase-01/js/scripts.js
@@ -1,2 +1,47 @@
-console.log("Mi nombre es Alvaro");
-console.log("Tengo 36 años");
+
+// Mostrando un mensaje en la consola del navegador
+// console.log("Mi nombre es Alvaro");
+// console.log("Tengo 36 años");
+
+// // Variables 
+// // let edad;
+// // edad = 36;
+// // console.log("Mi edad es: ", edad);
+
+// let edad = 36;
+// console.log("Mi edad es: ", edad);
+
+// edad = 37;
+// console.log("Mi edad actualizada es: ", edad);
+
+// let nombre = "Alvaro";
+// console.log("Mi nombre es: ", nombre);
+
+// let apellido = "Benitez";
+// console.log("Mi apellido es: ", apellido);  
+
+// console.log(`Mi nombre es: ${nombre} ${apellido}`);
+
+
+// // Constantes
+
+// const nombreCompleto = `${nombre} ${apellido}`;
+
+
+// const PAIS = "España";
+// console.log("Mi país es: ", PAIS);
+
+// const IVA = 0.21;
+// console.log("El IVA es: ", IVA);
+
+// const IVA_REDUCIDO = IVA - 0.17;
+// console.log("El IVA reducido es: ", IVA_REDUCIDO);
+
+// Tipos de datos primitivos
+
+let nombre = "Alvaro"; // String
+let edad = 36; // Number
+let esDesarrollador = true; // Boolean // CamelCase
+let direccion; // Undefined
+
+
