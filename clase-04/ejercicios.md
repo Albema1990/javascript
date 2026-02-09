703503dc13ad2cde8d14d7af60a316dce22cfe0f
Álvaro Benitez Mayorga <alvarobenitezmayorga@gmail.com>
2026-02-09 19:19:35 +0100

    clase 4

---
+++ clase-04/ejercicios.md
@@ -0,0 +1,24 @@
+# Ejercicios clase 04
+
+## Funcion
+
+1. Crear una funcion que muestre "Hola, mundo"
+2. Llamarla dos veces
+3. Cambiar el mensaje y volver a ejecutar
+
+## Parametros y argumentos
+
+1. Crear una funcion que reciba una edad y la muestre
+2. Crear una funcion que reciba dos numeros y los reste
+3. Probar distintos valores
+
+## Return
+
+1. Crear una funcion que devuelva un numero fijo
+2. Guardar el valor retornado en una variable
+3. Mostrar ese valor por consola
+
+## Función expresada
+
+1. Crear una funcion expresada que muestre un mensaje.
+2. Llamarla desde la variable
--- clase-04/js/scripts.js
+++ clase-04/js/scripts.js
@@ -1,7 +1,49 @@
 //funcion declarada
 
-function saludar (){
-    console.log("Hola, Bienvenido a la clase de JavaScript");
-}
+// function saludar (){
+//     console.log("Hola, mundo");
+// }
 
-saludar();
+// saludar();
+// // saludar();
+
+// parametros y argumentos
+
+// function saludar (nombre){
+//     console.log("Hola, " + nombre);
+// }
+
+// saludar("Alvaro");
+
+// function sumar (num1, num2) {
+//     const suma = num1 + num2;
+//     console.log("La suma es: " + suma);
+// }
+
+// sumar(5, 10);
+
+//
+
+// function multiplicar(num1, num2) {
+//   return num1 * num2;
+// }
+
+// const producto = multiplicar(5, 10);
+
+// console.log("El producto es: " + producto);
+
+
+// function multiplicar(num1, num2) {
+//     const producto = num1 * num2;
+//     return producto;
+// }
+
+// console.log(multiplicar(5, 10));
+
+// const mensaje = function(mensaje) {
+//     return mensaje;
+// }
+
+// console.log(mensaje("Hola, mundo"));
+
+
